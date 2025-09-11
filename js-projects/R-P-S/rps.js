4ae3b2830d683c307521f7f282e667db821ef26d
akshay-095 <sonu.ay95@gmail.com>
2025-09-11 23:08:40 +0530

    completed

--- js-projects/R-P-S/rps.js
+++ js-projects/R-P-S/rps.js
@@ -1,14 +1,78 @@
 const choice=['rock','paper','scissor'];
-let count=0;
+const rdisplay=document.getElementById('round');
+const userscore=document.getElementById('uscore');
+const comscore=document.getElementById('cscore');
+const resultdisplay=document.getElementById('result');
+let count=1;
+let uscore=0;
+let cscore=0;
+rdisplay.textContent=`Round:${count}`;
+userscore.textContent=`user score:${uscore}`;
+comscore.textContent=`com score:${cscore}`;
+function round(user,com){
+    let result='';
+    console.log(user,com);
+    if(user==com){
+        result='draw';
+        console.log(result);
+    }
+    else{
+        switch(user){
+            case 'rock':
+                result = com==='scissor'? 'WINNER': 'LOSSER';
+                console.log(result);
+                break;
+            case 'scissor':
+                result = com==='paper'? 'WINNER': 'LOSSER';
+                console.log(result);
+                break;
+            case 'paper':
+                result = com==='rock'? 'WINNER': 'LOSSER';
+                console.log(result);
+                break;
+        }
+    }
+    switch(result){
+        case 'WINNER':
+            uscore++;
+            comscore.classList.remove('winner');
+            comscore.classList.add('loser');
+            userscore.classList.remove('loser');
+            userscore.classList.add('winner');
+            resultdisplay.classList.remove('loser');
+            resultdisplay.classList.add('winner');
+            break;
+        case 'LOSSER':
+            cscore++;
+            userscore.classList.remove('winner');
+            userscore.classList.add('loser');
+            comscore.classList.remove('loser');
+            comscore.classList.add('winner');
+            resultdisplay.classList.remove('winner');
+            resultdisplay.classList.add('loser');
+            break;
+        default:
+            userscore.classList.remove('winner','loser');
+            resultdisplay.classList.remove('winner','loser');
+            cscore++;
+            uscore++;
+            break;
+    }
+    userscore.textContent=`user score:${uscore}`;
+    comscore.textContent=`com score:${cscore}`;
+    resultdisplay.textContent=result;
+}
 function play(user){
-    console.log(user);
-    while(count!=5){
+    if(count<6){
+        count++;
         const index=Math.floor(Math.random()*3);
         const com=choice[index];
-        console.log(user,com);
-        count++;
+        round(user,com);
+        if(count!=6){
+            rdisplay.textContent=`Round:${count}`;
+        }
     }
-}
-function round(user,com){
-    console.log(user,com);
+    else{
+        location.reload();
+    }
 }
