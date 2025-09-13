2908ddf5158347b06c7d15ec97442955d25470ec
akshay-095 <sonu.ay95@gmail.com>
2025-09-13 21:53:11 +0530

    use foreach method when dealing with nodelist and append child to add it to div

---
+++ js-projects/etch-a-sketch/e-a-s.js
@@ -0,0 +1,23 @@
+const container=document.querySelector('.container');
+function create(){
+    for(let i=0;i<16;i++){
+        const row=document.createElement('div');
+        row.classList.add('row');
+        for(let j=0;j<16;j++){
+            const square=document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+}
+create();
+const squares=document.querySelectorAll('.square');
+console.log(squares);
+squares.forEach(square=>{
+    square.addEventListener('mouseover',()=>{
+        square.style.background='yellow';
+    });
+});
+/*
+*/
