f6d279d082ba63bbc6a4f1ac3e408d75ebc86c45
akshay-095 <sonu.ay95@gmail.com>
2025-09-14 23:24:00 +0530

    added function to reload the page on clicking reset and the function start creates the grid as required by the user

--- js-projects/etch-a-sketch/e-a-s.js
+++ js-projects/etch-a-sketch/e-a-s.js
@@ -1,9 +1,12 @@
 const container=document.querySelector('.container');
-function create(){
-    for(let i=0;i<16;i++){
+function rel(){
+    location.reload();
+}
+function create(num){
+    for(let i=0;i<num;i++){
         const row=document.createElement('div');
         row.classList.add('row');
-        for(let j=0;j<16;j++){
+        for(let j=0;j<num;j++){
             const square=document.createElement('div');
             square.classList.add('square');
             row.appendChild(square);
@@ -11,13 +14,21 @@ function create(){
         container.appendChild(row);
     }
 }
-create();
-const squares=document.querySelectorAll('.square');
-console.log(squares);
-squares.forEach(square=>{
-    square.addEventListener('mouseover',()=>{
+function start(){
+    const num=document.getElementById('num').value;
+    create(num);
+    const size=(960/num);
+    const squares=document.querySelectorAll('.square');
+    console.log(squares);
+    squares.forEach(square=>{
+        square.style.width=`${size}px`;
+        square.style.height=`${size}px`;
+    })
+    squares.forEach(square=>{
+        square.addEventListener('mouseover',()=>{
         square.style.background='yellow';
     });
 });
-/*
-*/
+}
+
+
